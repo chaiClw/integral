3d507a7f2f8ad39be5bda5da45b5e2c0c73fbc3f
柴立卫 <15536902091@163.com>
2020-11-24 17:03:04 +0800

    更改优惠券规则model

--- src/model/IntegralGoodsOfflineRuleModel.php
+++ src/model/IntegralGoodsOfflineRuleModel.php
@@ -5,5 +5,11 @@ namespace integral\model;
 
 class IntegralGoodsOfflineRuleModel extends BaseModel
 {
-    public $table = 'integral_goods_offline_rule';
+    public $table = 'library.integral_goods_offline_rule';
+
+    public function __construct($data = []) {
+        parent::__construct($data);
+        $env = $_SERVER['ENV'] ?? '';
+        $this->table = $env . $this->table;
+    }
 }
